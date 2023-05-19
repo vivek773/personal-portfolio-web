dd7bd1cd1c13bc913e4dc6ef4819c56ea98923f1
Vivek Kumar Singh <72679597+vivek773@users.noreply.github.com>
2023-05-19 23:58:42 +0530

    INV-1 Pages setup

---
+++ personal-portfolio/src/pages/About/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <React.Fragment>
+         <div>
+      <h1>This is my about page</h1>
+    </div> 
+    </React.Fragment>
+  
+  )
+}
+
+export default About
---
+++ personal-portfolio/src/pages/Contact/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <React.Fragment>
+        <div><h1>
+            This is my contact page
+            </h1></div>
+      </React.Fragment>
+    </div>
+  )
+}
+
+export default Contact
