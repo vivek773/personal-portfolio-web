e077dac0f1b33884297fc5f82133397238127fb6
Vivek Kumar Singh <72679597+vivek773@users.noreply.github.com>
2023-05-20 23:07:37 +0530

    INV- Navbar added

--- personal-portfolio/src/pages/About/index.tsx
+++ personal-portfolio/src/pages/About/index.tsx
@@ -1,8 +1,12 @@
+import Navbar from "@/components/navbar";
 import React from "react";
 
 const About = () => {
   return (
     <React.Fragment>
+      {/* Navbar */}
+      <Navbar />
+
       <div className="about">
         <h1>This is my about page</h1>
       </div>
--- personal-portfolio/src/pages/Work/index.tsx
+++ personal-portfolio/src/pages/Work/index.tsx
@@ -1,8 +1,12 @@
+import Navbar from "@/components/navbar";
 import React from "react";
 
 const Work = () => {
   return (
     <React.Fragment>
+      {/* Navbar */}
+      <Navbar />
+
       <div className="work">
         <h1>This is my Work page</h1>
       </div>
