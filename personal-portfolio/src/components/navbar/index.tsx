4dd303e688a6a76123f0f7179103a5ee9b22fb13
Vivek Kumar Singh <72679597+vivek773@users.noreply.github.com>
2023-05-22 23:37:33 +0530

    Added reusable classes

--- personal-portfolio/src/components/navbar/index.tsx
+++ personal-portfolio/src/components/navbar/index.tsx
@@ -1,4 +1,5 @@
 // Navbar
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
 import React, { use, useReducer } from "react";
 
@@ -13,11 +14,7 @@ const Navbar = () => {
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" className="flex items-center">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8 mr-3"
-                alt="Flowbite Logo"
-              />
+            <FontAwesomeIcon icon="fa-solid fa-user"  size="2x" className="mr-5"/>
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 Portfolio
               </span>
--- personal-portfolio/src/pages/_app.tsx
+++ personal-portfolio/src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import "../styles/index.scss";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import type { AppProps } from "next/app";
+
+library.add(fas);
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
